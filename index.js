fed643e9920293fef398104f4f090a8a65730604
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-18 14:41:26 +0500

    tex-bot

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ client.on('message', async message => {
 client.on('message', async message => {
     if(message.content.startsWith("$help")) {
         const embed = new Discord.MessageEmbed()
-        .setDescription("Please write what type of help you want. Following modules are: economy, meme, joke, covid, ")
+        .setDescription("Please write what type of help you want. Following modules are: economy, meme, joke, cnjoke, covid, support, invite")
         .setFooter("Write $help {moduleName}")
         message.channel.send(embed)
     }
@@ -271,6 +271,38 @@ client.on('message', async message => {
     }
 })
 
+client.on('message', async message => {
+    if(message.content.startsWith("$help cnjoke")) {
+        const jokeembed = new Discord.MessageEmbed()
+        .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
+        .setThumbnail("https://i.imgur.com/ZHUpgyz.png")
+        .setTitle("Help for CnJoke")
+        .setDescription("Write **$cnjoke** to generate a joke!")
+        message.channel.send(jokeembed)
+    }
+})        
+
+client.on('message', async message => {
+    if(message.content.startsWith("$help support")) {
+        const jokeembed = new Discord.MessageEmbed()
+        .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
+        .setThumbnail("https://i.imgur.com/ZHUpgyz.png")
+        .setTitle("Help for Support")
+        .setDescription("Write **$support** to join the support server")
+        message.channel.send(jokeembed)
+    }
+})
+
+client.on('message', async message => {
+    if(message.content.startsWith("$help invite")) {
+        const jokeembed = new Discord.MessageEmbed()
+        .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
+        .setThumbnail("https://i.imgur.com/ZHUpgyz.png")
+        .setTitle("Help for Invite")
+        .setDescription("Write **$invite** to get invite link for the bot")
+        message.channel.send(jokeembed)
+    }
+})
 
 //help command
 
