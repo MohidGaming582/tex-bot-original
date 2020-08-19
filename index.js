aabf30b7f21ad4a94c64f467e9d74d658a0cf2df
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-19 09:37:55 +0500

    tex-bot

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command)
+    client.commands.set(command.name, command);
 }
 
 const randomPuppy = require('random-puppy');
