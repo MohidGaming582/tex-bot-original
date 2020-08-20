2eab0217e65624fc52d9f0ba07005e40416f4ab9
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-20 15:41:42 +0500

    tex-bot

--- index.js
+++ index.js
@@ -322,6 +322,7 @@ client.on('message', async message => {
         const executeembed = new Discord.MessageEmbed()
         .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
         .setColor("GREEN")
+        .setThumbnail("https://i.imgur.com/ZHUpgyz.png")
         .setDescription(`<@&745126451044679800>`)
         .addField("**Bot Update V1.1** *(Alpha Version)*", "\u200B")
         .addField("\u200B", "\u200B")
