5be4a7f6a71d4bc01838c35f9e535b42cbe343d4
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-20 15:38:41 +0500

    tex-bot

--- index.js
+++ index.js
@@ -321,11 +321,12 @@ client.on('message', async message => {
     if(message.content === "$executechangelogv1.1") {
         const embed = new Discord.MessageEmbed()
         .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
+        .setColor("GREEN")
         .setDescription(`<@&745126451044679800>`)
         .addField("**Bot Update V1.1** *(Alpha Version)*", "\u200B")
         .addField("\u200B", "\u200B")
         .addField("``Changelog v1.1``", "\u200B")
-        .addField("```1-) Changed the help command. Now better.", "\u200B")
+        .addField("```1-) Changed the help command. Now better.```", "\u200B")
         .setFooter("Thats all in v1.1 changelog")
         .setTimestamp()
         message.channel.send(embed)
