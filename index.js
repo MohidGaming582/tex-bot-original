9d0ba027a50e1d42114445da7a639f88e236d7f0
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-26 09:34:00 +0500

    tex-bot

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ client.on('message', async message => {
 })        
 
 client.on('message', async message => {
-    if(message.content.startsWith(`${PREFIX}help support ${args[0]}`)) {
+    if(message.content.startsWith(`${PREFIX}help support ${args[1]}`)) {
         const jokeembed = new Discord.MessageEmbed()
         .setAuthor("Tex Bot", "https://i.imgur.com/ZHUpgyz.png")
         .setThumbnail("https://i.imgur.com/ZHUpgyz.png")
