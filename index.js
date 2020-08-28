0e95bf8e233bf5be4d7e877d550285702ab43d7d
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-28 21:06:50 +0500

    tex-bot

--- index.js
+++ index.js
@@ -122,51 +122,6 @@ if (message.content.startsWith("$beg")) {
     } 
 }
 
-if(!message.content.startsWith('$pay')) {  
-
-let user = message.mentions.members.first() 
-
-let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
-
-let embed1 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:cross: Mention someone to pay`);
-
-if (!user) {
-    return message.channel.send(embed1)
-}
-let embed2 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:cross: Specify an amount to pay`);
-
-if (!args[1]) {
-    return message.channel.send(embed2)
-}
-let embed3 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:cross: You can't pay someone negative money`);
-
-if (message.content.includes('-')) { 
-    return message.channel.send(embed3)
-}
-let embed4 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:cross: You don't have that much money`);
-
-if (member < args[1]) {
-    return message.channel.send(embed4)
-}
-
-let embed5 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:white_check_mark:  You have payed ${user.user.username} ${args[1]} coins`);
-
-message.channel.send(embed5)
-db.add(`money_${message.guild.id}_${user.id}`, args[1])
-db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
-
-
-} //please work now
 })
 
 
