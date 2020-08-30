cf5c9e022bcd73cd52f97517529ed1c33fc84f24
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-08-30 19:33:07 +0500

    tex-bot

--- index.js
+++ index.js
@@ -121,37 +121,6 @@ if (message.content.startsWith("$beg")) {
     } 
 }
 
-if(!message.content.startsWith('$pay'))return;
-
-let user = message.mentions.members.first() 
-
-let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
-
-if (!user) {
-    return message.channel.send("❌ You need to mention someone to pay")
-}
-
-if (!args[1]) {
-    return message.channel.send("❌ You need to specify an amount to pay")
-}
-
-if (message.content.includes('-')) { 
-    return message.channel.send("❌ You can\'t pay negative money to someone")
-}
-
-if (member < args[1]) {
-    return message.channel.send("❌ You dont have that much money")
-}
-
-let embed5 = new Discord.MessageEmbed()
-.setColor("#FFFFFF")
-.setDescription(`:white_check_mark:  You have payed ${user.user.username} ${args[1]} coins`);
-
-message.channel.send(embed5)
-db.add(`money_${message.guild.id}_${user.id}`, args[1])
-db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
-
-
 })
 
 //economy bot
