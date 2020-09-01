cd23c0ab7cdf77c0298ae6622b22126422b07587
MohidGaming582 <67559813+MohidGaming582@users.noreply.github.com>
2020-09-01 11:52:24 +0500

    tex-bot

--- index.js
+++ index.js
@@ -121,6 +121,87 @@ if (message.content.startsWith("$beg")) {
     } 
 }
 
+if (message.content.startsWith("$pay")) {
+    let user = message.mentions.members.first() 
+
+    let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
+  
+    let embed1 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`:x: Mention someone to pay`);
+  
+    if (!user) {
+        return message.channel.send(embed1)
+    }
+    let embed2 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`:x: Specify an amount to pay`);
+    
+    if (!args[1]) {
+        return message.channel.send(embed2)
+    }
+  
+      if (isNaN(args[1])) return message.channel.send({embed: {
+                      color: 16734039,
+                      description: "You must enter the amount of money to pay!"
+                  }})
+  
+    let embed3 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`:x: You can't pay someone negative money`);
+  
+    if (message.content.includes('-')) { 
+        return message.channel.send(embed3)
+    }
+    let embed4 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`:x: You don't have that much money`);
+  
+    if (member < args[1]) {
+        return message.channel.send(embed4)
+    }
+  
+    let embed5 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`:white_check_mark:  You have payed ${user.user.username} ${args[1]} coins`);
+  
+    message.channel.send(embed5)
+    db.add(`money_${message.guild.id}_${user.id}`, args[1])
+    db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
+  
+}
+
+
+let hourlytimeout = 86400000
+let hourlyamount = 5
+
+const embed6 = new Discord.MessageEmbed()
+.setDescription(":x: This command can only be executed by patreon supporters")
+.setColor("RED")
+
+if (message.content.startsWith("hourly")) {
+    if (message.member.roles.has('740081399318446089')) return message.channel.send(embed6)
+    let hourly = await db.fetch(`hourly_${message.author.id}`);
+
+    if (hourly != null && hourlytimeout - (Date.now() - hourly) > 0) {
+        let hourlytime = ms(hourlytimeout - (Date.now() - hourly));
+        message.channel.send(`You already collected your daily coins, you can come back in **${hourlytime.hours}h ${hourlytime.minutes}m ${hourlytime.seconds}s**`)
+
+
+    } else {
+        let hourlyembed = new Discord.MessageEmbed()
+        .setAuthor(`Only For Patreon Supporters`, message.author.displayAvatarURL())
+        .setColor("GREEN")
+        .setDescription(`**Hourly Rewards**`)
+        .addField(`Collected`, amount)
+        message.channel.send(hourlyembed)
+
+        db.add(`money_${message.author.id}`, hourlyamount)
+        db.add(`hourly_${message.author.id}`, Date.now())
+    }
+}
+
+
 })
 
 //economy bot
